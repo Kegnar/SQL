93bb28f1442876fe68501d33bb7b5b76bd8e4172
Kegnar <mitriy.exe@gmail.com>
2026-02-25 23:02:55 +0400

    работа на паре

--- DDL/SQLQuery1 - sp_SelectScheduleForGroup.sql
+++ DDL/SQLQuery1 - sp_SelectScheduleForGroup.sql
@@ -1,6 +1,6 @@
 USE PV_521_Import
 GO
-CREATE PROCEDURE sp_SelectScheduleFor
+ALTER PROCEDURE sp_SelectScheduleFor
 	@group	AS NCHAR(10)
 AS
 BEGIN
@@ -16,6 +16,9 @@ BEGIN
 		,[Преподаватель] = FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name)
 		,[Статус] = IIF(spent = 1, N'Проведено',N'Запланировано')
 
-	FROM Schedule ,Groups ,Teachers ,Disciplines 
-	WHERE [group] = @group AND discipline = discipline_id AND teacher = teacher_id
+	FROM Schedule ,Groups ,Teachers ,Disciplines
+	WHERE [group] = group_id
+		AND [group] = @group_id
+		AND discipline = discipline_id
+		AND teacher = teacher_id
 END;
